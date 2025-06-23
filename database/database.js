224ae15a4956e4a9ddfb1e3bf637763d202f37a5
Aprimesoft <morenabaltazar38@gmail.com>
2025-06-23 19:05:18 +0000

    processto finalizado

--- database/database.js
+++ database/database.js
@@ -1,7 +1,9 @@
+const {config }= require('dotenv').config()
 const mongoose =require('mongoose')
+
 //importançãoes
 const db = mongoose.connect(
-  'mongodb+srv://baltazar23011998:baltazar23011998@bdpixelprint.mik9v3g.mongodb.net/?retryWrites=true&w=majority&appName=bdpixelprint'
+  `mongodb+srv://${process.env.USER}:${process.env.PASS}@bdpixelprint.mik9v3g.mongodb.net/?retryWrites=true&w=majority&appName=bdpixelprint`
 ).then(()=>{
     console.log('base dados conectado com sucesso!')
 }).catch((error)=>{
