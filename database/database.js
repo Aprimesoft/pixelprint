1c5d9ba4faf9357c976a5c7600dd4afbdd5ca06b
Aprimesoft <morenabaltazar38@gmail.com>
2025-06-23 19:08:12 +0000

    processto finalizado

--- database/database.js
+++ database/database.js
@@ -3,7 +3,7 @@ const mongoose =require('mongoose')
 
 //importançãoes
 const db = mongoose.connect(
-  `mongodb+srv://${process.env.USER}:${process.env.PASS}@bdpixelprint.mik9v3g.mongodb.net/?retryWrites=true&w=majority&appName=bdpixelprint`
+  `mongodb+srv://${process.env.USER}:${process.env.PASS}@${process.env.BD}.mik9v3g.mongodb.net/?retryWrites=true&w=majority&appName=bdpixelprint`
 ).then(()=>{
     console.log('base dados conectado com sucesso!')
 }).catch((error)=>{
